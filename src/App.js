7b7b4d6404bc81b180dce5c3ce458cd18c15ca8d
Christopher McCarthy <chris@Christophers-iMac.local>
2019-10-10 23:49:57 -0700

    Added Header, Player and Counter to scoreboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <h1>Game Score Board</h1>
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Player: 1</span>
+    </header>
+  );
+}
+
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        Chris
+      </span>
+
+      <Counter />
+    </div>
+  );
+}
+
+const Counter = () => {
+  return(
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">13</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+}
+
+const App = () => {
+  return(
+    <div className='scoreboard'>
+      <Header />
+
+      {/* Players List */}
+      <Player />
+
     </div>
   );
 }
